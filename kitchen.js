c51a443c48fb94d7c7709ea41e855c11b76179a8
Trixek24 <121521201+Trixek24@users.noreply.github.com>
2023-11-12 18:23:16 -0500

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,35 @@
+var status = ""
+
+function preload()
+{
+    img = loadImage("kitchen.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        console.log(results);
+        objects = results;
+    }
+}
